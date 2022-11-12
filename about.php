71dada4ba7bf0bb3aa0cbcbe1941c888ea73fd56
ngocsang2002 <114401932+ngocsang2002@users.noreply.github.com>
2022-11-12 07:18:01 +0700

    about.php nhe

--- about.php
+++ about.php
@@ -27,31 +27,31 @@
                     <p>Medical-VN hoạt động từ 7h đến 22h hằng ngày, hoặc bất cứ khi nào bận cần có thể liên hệ chúng tôi qua email để được xử lý vào thời điểm sớm nhất</p>
                 </div>
                 <div class="row">
-                    <img src="images/bg/GrabFood.jpg" alt="">
+                    <img src="images/bg/banner.png" alt="">
 
-                    <h3>Những nhà hàng, quán ăn nào trong khu vực của tôi giao hàng qua GrabFood?</h3>
-                    <p>Để xem danh sách các nhà hàng, quán ăn có giao hàng qua GrabFood bạn chỉ cần nhập địa chỉ của mình trên ứng dụng. Để đảm bảo đồ ăn nóng hổi, tươi ngon và được giao đến bạn nhanh chóng, GrabFood sẽ quét vị trí của bạn và gợi ý danh sách nhà hàng, quán ăn ở gần vị trí bạn nhất.</p>
+                    <h3>Những thiết bị y tế nào trong Website của tôi bán hàng qua Medical-VN</h3>
+                    <p>Để xem danh sách các thiết bị y tế có bán qua Medical-VN bạn chỉ cần nhập địa chỉ website “xxx” của chúng tôi. Và khi bạn đặt hàng, mua hàng thì để đảm bảo thiết bị y tế không bị móp méo và được giao đến bạn nhanh chóng, Medical sẽ dựa vào địa chỉ bạn cung cấp mà đóng gói kĩ càng, giao tới trong vòng 2h hoặc 1 ngày sau khi đặt hàng thành công.</p>
                 </div>
                 <div class="row">
                     <h3>Tôi có thể thanh toán bằng tiền mặt không?</h3>
                     <p>Có nhé!</p>
                 </div>
                 <div class="row">
-                    <h3>Tôi có thể thanh toán bằng GrabPay không?</h3>
-                    <p>Hiện tại bạn chưa thể thanh toán GrabFood bằng GrabPay. Chúng tôi đang cố gắng để áp dụng phương thức thanh toán này cho dịch vụ GrabFood trong thời gian sớm nhất</p>
+                    <h3>Tôi có thể thanh toán bằng thẻ không?</h3>
+                    <p>Hiện tại bạn có thể thanh toán đơn hàng ở Medical bằng các thẻ ngân hàng bằng cách chuyển khoản vào TK với STK:22110299999, Chủ sở hữu Medical-VN. Chúng tôi đang cố gắng để áp dụng phương thức thanh toán này cho dịch vụ một cách tự động.</p>
                 </div>
                 <div class="row">
                     <h3>Chi phí được tính như thế nào?</h3>
-                    <p>Chi phí hiển thị trên ứng dụng bao gồm chi phí của phần ăn theo đơn giá của nhà hàng và phí vận chuyển.</p>
+                    <p>Chi phí hiển thị trên website bao gồm chi phí của thiết bị y tế theo đơn giá của Medical-VN và chưa tính phí vận chuyển.</p>
                 </div>
                 <div class="row">
-                    <h3>Tôi có thể đặt giao nhận những món ăn nào qua GrabFood?</h3>
-                    <p>Danh sách đa dạng các món ăn của chúng tôi bao gồm: món Việt, Tây, Á, món theo phong cách Fusion,… có thể phục vụ cho mọi nhu cầu ăn uống của bạn.</p>
+                    <h3>Tôi có thể đặt giao nhận những thiết bị nào qua Medical-VN?</h3>
+                    <p>Danh sách đa dạng các thiết bị y tế của chúng tôi bao gồm: Nhiệt kế, Dụng cụ kiểm tra Covid-19, Máy xông khí dung, Máy massage,… có thể phục vụ cho một số nhu cầu của bạn.</p>
                 </div>
-                <div class="row">
+                <!-- <div class="row">
                     <h3>Tôi có thể tìm thấy những nhà hàng, quán ăn nào trong khu vực của mình?</h3>
                     <p>Danh sách nhà hàng, quán ăn được sắp xếp dựa theo khoảng cách và thời gian giao hàng dự kiến từ Địa chỉ giao thức ăn đến vị trí của bạn.</p>
-                </div>
+                </div> -->
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.280539447332!2d107.29400661486774!3d16.762712588455788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x80f122cab2275a42!2zTmd1eeG7hW4gxJDEg25nIFRow6BuaA!5e0!3m2!1svi!2s!4v1629007864673!5m2!1svi!2s" width="550" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/jJoFCFcJHsI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </section>
