4e0fffabd567f34175895d538f85c7e8c9907282
ngocsang2002 <114401932+ngocsang2002@users.noreply.github.com>
2022-11-12 08:12:02 +0700

    details.php

--- details.php
+++ details.php
@@ -24,7 +24,7 @@ if (isset($_GET['id'])) {
         <section class="search-quan">
                 <i class="fas fa-search"></i>
                 <form action="thucdon.php" method="GET">
-                    <input name="search" type="text" placeholder="Tìm món hoặc thức ăn">
+                    <input name="search" type="text" placeholder="Tìm thiết bị">
                 </form>
             </section>
         </div>
