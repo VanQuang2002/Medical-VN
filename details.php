9043b79211bbf9eab307a453d35ffc894e8ab42d
ngocsang2002 <114401932+ngocsang2002@users.noreply.github.com>
2022-11-12 08:10:27 +0700

    details.php

--- details.php
+++ details.php
@@ -105,7 +105,7 @@ if (isset($_GET['id'])) {
             </section>
             <section class="restaurants">
                 <div class="title">
-                    <h1>Thực đơn tại quán <span class="green">Thành Coffee</span></h1>
+                    <h1>Thiết bị tại công ty <span class="green">Medical-VN</span></h1>
                 </div>
                 <div class="product-restaurants">
                     <div class="row">
